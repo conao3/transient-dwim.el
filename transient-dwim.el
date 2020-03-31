c553c5becea0413074681c750b65e5588842d24c
conao3 <conao3@gmail.com>
2020-04-01 01:56:07 +0900

    add description

--- transient-dwim.el
+++ transient-dwim.el
@@ -19,11 +19,22 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 ;;; Commentary:
 
-;; Useful preset transient commands
+;; Using transient to display the preconfigured transient dispatcher
+;; for your situation (major-mode or installed packages).
+
+;; By installing this package, if you want to know what features
+;; exist in major-mode for the first time, or if you want to use
+;; a package you can't remember keybindings for, pressing M-= (if
+;; you're using the recommended settings) will be your first
+;; step.
+
+;; You don't have to remember the cumbersome keybindings since
+;; now.  Just look at the neatly aligned menu and press the key
+;; that appears.
 
 ;; To Use this package, simply add this to your init.el:
 ;;   (define-key global-map (kbd "M-=") 'transient-dwim-dispatch)
